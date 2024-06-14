d345be2389fb49a2d713efe5de8865bf47a9b8ca
Bruna Carrijo <brunabcarrijoo@gmail.com>
2024-06-14 18:14:13 -0300

    última atualização do  JS

--- script-crud.js
+++ script-crud.js
@@ -4,8 +4,12 @@ const textArea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
 const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description');
 
-const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+const btnRemoverConcluidas = document.querySelector('#btn-remover-concluidas');
+const btnRemoverTodas = document.querySelector('#btn-remover-todas');
+
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 let tarefaSelecionada = null;
+let liTarefaSelecionada = null;
 
 function atualizarTarefas (){
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
@@ -53,24 +57,28 @@ function criarElementoTarefa(tarefa){
     li.append(paragrafo);
     li.append(botao);
 
-    
-    li.onclick = () => {
-        
-        document.querySelectorAll('.app__section-task-list-item-active')
-            .forEach(elemento =>{
+    if (tarefa.completa) {
+        li.classList.add('app__section-task-list-item-complete')
+        botao.setAttribute('disabled', 'disabled')
+    } else {
+        li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active').forEach(elemento =>{
                 elemento.classList.remove('app__section-task-list-item-active');
             })
             
         if (tarefaSelecionada == tarefa) {
             paragrafoDescricaoTarefa.textContent = ''
             tarefaSelecionada = null;
+            liTarefaSelecionada = null;
             return;
             
          }
         tarefaSelecionada = tarefa;
+        liTarefaSelecionada = li;
         paragrafoDescricaoTarefa.textContent = tarefa.descricao;
         
         li.classList.add('app__section-task-list-item-active');
+        }
     }
 
     return li
@@ -100,4 +108,25 @@ tarefas.forEach(tarefa => {
    ulTarefas.append(elementoTarefa);
 });
 
+document.addEventListener('FocoFinalizado', () => {
+    if (tarefaSelecionada && liTarefaSelecionada) {
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active');
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete');
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled');
+        tarefaSelecionada.completa = true;
+        atualizarTarefas();        
+    }
+});
+
+const removerTarefas = (somenteCompletas) => {
+    const seletor = somenteCompletas ? ".app__section-task-list-item-complete" : ".app__section-task-list-item"
+    document.querySelectorAll(seletor).forEach(elemento => {
+        elemento.remove()
+    })
+    tarefas = somenteCompletas ? tarefas.filter(tarefa => !tarefa.completa) : []
+    atualizarTarefas()
+}
+
+btnRemoverConcluidas.onclick = () => removerTarefas(true)
+btnRemoverTodas.onclick = () => removerTarefas(false)
 
